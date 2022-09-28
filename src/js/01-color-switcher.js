7f7d09a558fe5741eee1738a2fdd8835037c4b60
Marianna Takovaya <mariannatakovaya@gmail.com>
2022-09-28 10:55:00 +0300

    edits

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,23 @@
+const btn = document.querySelectorAll("button");
+let timerId;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function handleButtonClic(e) {
+  switch (true) {
+    case e.target.hasAttribute("data-start"):
+      e.target.setAttribute("disabled", "disabled");
+      timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+      break;
+    case e.target.hasAttribute("data-stop"):
+      clearInterval(timerId);
+      e.target.previousElementSibling.removeAttribute("disabled");
+      break;
+  }
+}
+
+[...btn].forEach((item) => item.addEventListener("click", handleButtonClic));
